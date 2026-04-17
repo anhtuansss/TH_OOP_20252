a07088d43f1ad41049f4d23a34d5e995536fd88a
Anh Tuans <131644634+anhtuansss@users.noreply.github.com>
2026-04-17 23:09:31 +0700

    Revert "Feature/search cart"

--- Lab03/AimsProject/src/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/DigitalVideoDisc.java
@@ -4,17 +4,10 @@ public class DigitalVideoDisc {
     private String director;   
     private int length;        
     private float cost;
-	
-  // Class variable to track the total number of DVDs created
-	private static int nbDigitalVideoDiscs = 0; 
-	// Instance variable for unique ID
-  private int id;
-  
+    
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
-		nbDigitalVideoDiscs++; // Increment class counter
-        this.id = nbDigitalVideoDiscs; // Assign unique ID
 	}
 	
 	public DigitalVideoDisc(String title, String category, float cost) {
@@ -22,8 +15,6 @@ public class DigitalVideoDisc {
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
-		nbDigitalVideoDiscs++; // Increment class counter
-        this.id = nbDigitalVideoDiscs; // Assign unique ID
 	}
 
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
@@ -32,8 +23,6 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
-		nbDigitalVideoDiscs++; // Increment class counter
-        this.id = nbDigitalVideoDiscs; // Assign unique ID
 	}
 
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -43,25 +32,14 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
-		nbDigitalVideoDiscs++; // Increment class counter
-        this.id = nbDigitalVideoDiscs; // Assign unique ID
-	}
-
-	@Override 
-	public String toString() {
-		return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + "$";
 	}
 	
 	public void setTitle(String title) {
     	this.title = title;
 	}
 
-	public void setTitle(String title) {
+	public void setTitle (String title) {
 		this.title = title;
-	}
-
-	public boolean isMatch(String title) {
-		return this.getTitle().equalsIgnoreCase(title);
 	}
 
 	public String getTitle() {
@@ -79,9 +57,5 @@ public class DigitalVideoDisc {
 	public float getCost() {
 		return cost;
 	}        
-
-	public int getId() {
-		return id;
-	}	
 }
 
