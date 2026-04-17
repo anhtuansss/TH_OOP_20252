6e02acf66059c8ce2b53166e8eb76b6da4de6569
emchjp <bui.tuan240606@gmail.com>
2026-04-17 23:43:36 +0700

    Xoa submodule loi va khoi phuc Lab 1, Lab 2

---
+++ Lab01/TwoMatrices.java
@@ -0,0 +1,35 @@
+package Lab01;
+
+public class TwoMatrices {
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int[][] B = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int rows = A.length;       
+        int cols = A[0].length;    
+
+        int[][] result = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {           
+            for (int j = 0; j < cols; j++) {      
+                result[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        
+        System.out.println("Result: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(result[i][j] + "\t"); 
+            }
+            System.out.println(); 
+        }
+    }
+}
