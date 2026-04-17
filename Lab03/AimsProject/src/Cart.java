d6cd602eef1407571eb34a24e9b89e8607ad818c
emchjp <bui.tuan240606@gmail.com>
2026-04-17 13:32:38 +0700

    Refactor repository structure and apply release flow

---
+++ Lab03/AimsProject/src/Cart.java
@@ -0,0 +1,61 @@
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0; 
+
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+	    if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+	        itemsOrdered[qtyOrdered] = disc;
+	        qtyOrdered++;
+	        System.out.println("The disc has been added");
+	    } else {
+	        System.out.println("The cart is almost full");
+	    }
+	}
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        for (int i = 0; i < dvdList.length; i++) {
+            if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+                itemsOrdered[qtyOrdered] = dvdList[i];
+                qtyOrdered++;
+                System.out.println("The disc " + dvdList[i].getTitle() + " has been added");
+            } else {
+                System.out.println("The cart is almost full. Cannot add " + dvdList[i].getTitle());
+                break; 
+            }
+        }
+    }
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
+    }
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+	    if (qtyOrdered == 0) {
+	        System.out.println("The cart is empty. Nothing to remove.");
+	        return;
+	    }
+	    
+	    for (int i = 0; i < qtyOrdered; i++) {
+	        if (itemsOrdered[i] == disc) { 
+	            for (int j = i; j < qtyOrdered - 1; j++) {
+	                itemsOrdered[j] = itemsOrdered[j + 1];
+	            }
+	            itemsOrdered[qtyOrdered - 1] = null; 
+	            qtyOrdered--;
+	            System.out.println("The disc has been removed");
+	            return;
+	        }
+	    }
+	    System.out.println("The disc is not in the cart");
+	}
+	
+	public float totalCost() {
+	    float total = 0;
+	    for (int i = 0; i < qtyOrdered; i++) {
+	        total += itemsOrdered[i].getCost();
+	    }
+	    return total;
+	}
+}
